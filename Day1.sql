6d751796ab1cf4f08508c585844b9854a2dec098
vishwanath <jeeragod2004@gmail.com>
2025-12-29 17:45:46 +0530

    In day1 i learned about how to create database and inserting values

---
+++ Day1.sql
@@ -0,0 +1,13 @@
+USE startersql;
+SELECT *from users;
+
+  -- Inserting values 
+--  INSERT INTO users(name,gender,email,date_of_birth) values
+-- ('rahul','male', 'rahul@gmail.com','2012-5-10'),
+--  ('ravi','male', 'ravi@gmail.com','2012-08-09'),
+-- ('raju','male', 'raju@gmail.com','2012-07-08');
+
+
+
+ -- SELECT * from users WHERE gender='male';
+SELECT * from users WHERE id>10;
